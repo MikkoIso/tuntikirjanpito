32215f226e539c58fac1c752930edc4dfba86511
tiinaVaisanen <tiina.vaisanen@awacademy.training>
2022-07-05 14:29:20 +0300

    lisätty aloitus- ja lopetusfunktiot

--- app.js
+++ app.js
@@ -1,17 +1,60 @@
 "use strict";
+import promptSync from "prompt-sync";
+const prompt = promptSync();
 
 const tuntikirjaus = {};
 
-async function aloituspvm() {}
+async function aloitus() {
+  let jatka = true;
+  do {
+    const pvm = prompt("Anna aloituspäivämäärä muodossa yyyy-mm-dd: ");
+    const klo = prompt("Anna aloituskellonaika muodossa hh:mm:ss: ");
+    const date = new Date(pvm + " " + klo);
+    if (isNaN(date)) {
+      // Tarkista, että validi päivämäärä: validoipvm()
+      console.log("Aloituspäivämäärän lisääminen ei onnistunut");
+    } else {
+      tuntikirjaus.aloitus = date;
+      console.log(`Aloituspäivämäärä  ja -aika: ${pvm} ${klo}`);
+      jatka = false;
+    }
+  } while (jatka);
+}
 
-async function aloitusklo() {}
+// Tarkista, että validi kellonaika
 
-async function lopetuspvm() {}
+async function lopetus() {
+  let jatka = true;
+  do {
+    const pvm = prompt("Anna lopetuspäivämäärä muodossa yyyy-mm-dd: ");
+    const klo = prompt("Anna lopetuskellonaika muodossa hh:mm:ss: ");
+    const date = new Date(pvm + " " + klo);
+    if (isNaN(date)) {
+      // Tarkista, että validi päivämäärä: validoipvm()
+      console.log("Lopetuspäivämäärän lisääminen ei onnistunut");
+    } else {
+      tuntikirjaus.lopetus = date;
+      console.log(`Lopetuspäivämäärä  ja -aika: ${pvm} ${klo} \n`);
+      jatka = false;
+    }
+  } while (jatka);
+}
 
-async function lopetusklo() {}
+// Tarkista, että validi päivämäärä: validoipvm()
+
+// Tarkista, että validi kellonaika
+// Tarkista että aloitus (pvm & klo) < lopetus (pvm & klo)
 
 async function projekti() {}
 
 async function selite() {}
 
-async function tulostakirjaus() {}
+async function kaynnista() {
+  console.log("Tervetuloa tuntikirjanpitoon! \n");
+  aloitus();
+  lopetus();
+  const obj = JSON.stringify(tuntikirjaus);
+  console.log(obj);
+}
+
+kaynnista();
---
+++ datasiirto.js
@@ -0,0 +1,7 @@
+"use strict";
+
+import pg from "pg";
+import client from pg;
+
+
+export async function lataaTietokantaan(data) {}
