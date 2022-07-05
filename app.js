fb318a32eb47c3c754a8c18a411398185d5754cd
Emmi L <emmi.lahtisalo@awacademy.training>
2022-07-05 12:05:36 +0300

    added some empty functions

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const tuntikirjaus = {};
+
+async function aloituspvm() {}
+
+async function aloitusklo() {}
+
+async function lopetuspvm() {}
+
+async function lopetusklo() {}
+
+async function projekti() {}
+
+async function selite() {}
+
+async function aloituspvm() {}
+
+async function aloitusklo() {}
+
+async function tulostakirjaus() {}
