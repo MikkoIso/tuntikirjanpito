b57076ab62926a05945c75a0c0a1d36d462a7609
tiinaVaisanen <tiina.vaisanen@awacademy.training>
2022-07-07 16:57:20 +0300

    pieniä viilauksia

--- raportti.js
+++ raportti.js
@@ -3,6 +3,7 @@ import { PublishCommand } from "@aws-sdk/client-sns";
 import { snsClient } from "./libs/snsClient.js";
 import { haeTietokannasta } from "./datasiirto.js";
 import viestipohja from "./viestipohja.js";
+import { getStackExports } from "./stackOutputs.js";
 
 let data = await haeTietokannasta();
 
@@ -10,16 +11,19 @@ const viesti = viestipohja(data);
 
 // console.log(viesti);
 
+const region = "eu-north-1";
+const stackName = "KuumatKinkut";
+
 // Set the parameters
 var params = {
   Message: viesti, // MESSAGE_TEXT
-  TopicArn: "arn:aws:sns:eu-north-1:235920682125:kuumakinkkukeskustelu", //TOPIC_ARN;
+  TopicArn: await getStackExports({ stackName, region }), //TOPIC_ARN;
 };
 
 export const publishToTopic = async () => {
   try {
     const data = await snsClient.send(new PublishCommand(params));
-    console.log("Success.", data);
+    console.log("Sähköposti lähetetty.", data);
     return data; // For unit tests.
   } catch (err) {
     console.log("Error", err.stack);
--- stackOutputs.js
+++ stackOutputs.js
@@ -28,5 +28,3 @@ export async function getStackExports(
   });
   return Object.values(exportsList)[0];
 }
-
-//console.log(await getStackExports({ stackName, region }));
